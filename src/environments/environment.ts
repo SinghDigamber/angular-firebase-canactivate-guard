e73325372fad064f67a3f5f8a731cb7b761793ed
Digamber <digamber@positronx.io>
2019-01-23 11:48:27 +0530

    Update security

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBNx4R1mHpGqb5ma7xiT2gDDcYWJ9kj00A",
-    authDomain: "angular-firebase-route-guard.firebaseapp.com",
-    databaseURL: "https://angular-firebase-route-guard.firebaseio.com",
-    projectId: "angular-firebase-route-guard",
-    storageBucket: "angular-firebase-route-guard.appspot.com",
-    messagingSenderId: "146816519669"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   }
 }
